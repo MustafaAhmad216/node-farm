44d0d9e38b464504828db895133deac79035ac40
Mustafa Ahmad Saied Ibrahim <mustafaahmad21666@gmail.com>
2024-03-05 23:31:31 +0200

    node-farm intialization

---
+++ modules/replaceTemplate.js
@@ -0,0 +1,12 @@
+module.exports = function(temp, product){
+    let output = temp.replace(/{% PRODUCTNAME %}/g, product.productName);
+    output = output.replace(/{% ID %}/g, product.id);
+    output = output.replace(/{% IMAGE %}/g, product.image);
+    output = output.replace(/{% FROM %}/g, product.from);
+    output = output.replace(/{% NUTRIENTS %}/g, product.nutrients);
+    output = output.replace(/{% QUANTITY %}/g, product.quantity);
+    output = output.replace(/{% PRICE %}/g, product.price);
+    output = output.replace(/{% DESCRIPTION %}/g, product.description);
+    if(!product.organic) output = output.replace(/{% NOTORGANIC %}/g, 'not-organic');
+    return output;
+}
