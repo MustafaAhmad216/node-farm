0582614e3dc207375e80a4b2a555ae5de5728a3b
Mustafa Ahmad Saied Ibrahim <mustafaahmad21666@gmail.com>
2024-03-06 01:16:27 +0200

    ...

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { create } = require('domain');
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
+
 /***********************************************************************/
 /***********************************************************************/
 
@@ -20,7 +22,7 @@ const replaceTemplate = require('./modules/replaceTemplate');
 
 //  *CALLBACK HELL*
 // fs.readFile('./txt/start.txt', 'utf-8', (err, data1)=>{
-    //     if(err) return console.log('ERROR! 💥💥💥');
+//     if(err) return console.log('ERROR! 💥💥💥');
 //     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2)=>{
 //         console.log(data2);
 //         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3)=>{
@@ -45,50 +47,50 @@ const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.h
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
-const apiData = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const apiData = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(apiData);
 
-const server = http.createServer((req, res)=>{
-    const {query, pathname} = url.parse(req.url, true);
-    
-    /*sending back a response depending on a specific request*/
-    //Overview Page
-    if (pathname === '/' || pathname === '/overview') {
-        res.writeHead(200, {'Content-type': 'text/html'});
-        const cardsHTML = dataObj.map((el)=>replaceTemplate(tempCard, el)).join('\n');
-        const output = tempOverview.replace('{% PRODUCT CARDS %}',cardsHTML);
-        res.end(output);
-    }
-    //Product Page
-    else if (pathname === '/product') {
-        const product = dataObj[query.id];
-        res.writeHead(200, {'Content-type': 'text/html'});        
-        const output = replaceTemplate(tempProduct, product);
-        res.end(output);
-    }
-    //API
-    else if (pathname === '/api') {
-        res.writeHead(200,{
-            'Content-type':'application/json'
-        });
-        res.end(apiData);
-    }
-    //NOT FOUND!
-    else{
-        res.writeHead(404, {
-            'Content-type': 'text/html',
-            'my-own-header': 'tifa-website',
-        });
-        res.end("<h1>PAGE NOT FOUND</h1>");
-    }
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+const server = http.createServer((req, res) => {
+	const { query, pathname } = url.parse(req.url, true);
+
+	/*sending back a response depending on a specific request*/
+	//Overview Page
+	if (pathname === '/' || pathname === '/overview') {
+		res.writeHead(200, { 'Content-type': 'text/html' });
+		const cardsHTML = dataObj.map((el) => replaceTemplate(tempCard, el)).join('\n');
+		const output = tempOverview.replace('{% PRODUCT CARDS %}', cardsHTML);
+		res.end(output);
+	}
+	//Product Page
+	else if (pathname === '/product') {
+		const product = dataObj[query.id];
+		res.writeHead(200, { 'Content-type': 'text/html' });
+		const output = replaceTemplate(tempProduct, product);
+		res.end(output);
+	}
+	//API
+	else if (pathname === '/api') {
+		res.writeHead(200, {
+			'Content-type': 'application/json',
+		});
+		res.end(apiData);
+	}
+	//NOT FOUND!
+	else {
+		res.writeHead(404, {
+			'Content-type': 'text/html',
+			'my-own-header': 'tifa-website',
+		});
+		res.end('<h1>PAGE NOT FOUND</h1>');
+	}
 });
-server.listen('8000', '127.0.0.1', ()=>{
-    console.log('Listening to request on port 8000...');
-})
+server.listen('8000', '127.0.0.1', () => {
+	console.log('Listening to request on port 8000...');
+});
 
 /***********************************************************************/
-// building a (very) Simple API 
-
+// building a (very) Simple API
 
 /***********************************************************************/
-
